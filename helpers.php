21270e7d508502ad951927256713d7c76685c1bf
donkey03 <zblackoez@gmail.com>
2024-05-24 23:02:55 -0300

    funcoes e variaveis

--- helpers.php
+++ helpers.php
@@ -1,3 +1,16 @@
 <?php
 
 echo '<br>arquivo de funcao';
+
+
+//função chamado no index!
+function saudacao(){
+
+    return 'Bem vindo(a), desgraçado(a)!';
+}
+
+// $texto <- isso é uma variavel
+function saudacao2($texto, $limite, $continue = '...'){
+
+    return $texto;
+};
--- index.php
+++ index.php
@@ -1,22 +1,16 @@
 <?php
-# ARQUIVO INDEX PADRÃO, MEU PAU NA SUA MÃO
-
-# INCLUDE -> inclui a pagina config, EXEMPLO: HEADERS/FOOTER
- // include 'config.php';
-
-#REQUIRE -> É MAIS SOBRE ALGO DE BANCO DE DADOS, ALGO IMPORTANTE PARA NOSSO SISTEMA.
-
-// require 'config.php'
-
-#REQUIRE_ONCE -> INCLUIU APENAS UMA VEZ / CARREGA APENAS 1 VEZ NO SYSTEMA
 
 require_once 'sistema/config.php';
 include_once 'helpers.php';
 
 echo '<p>Olá mundo / archive index</p>';
+//chamando a função do helpers!
+echo saudacao();
+//
 
+echo('<br>');
+echo saudacao2('Ah, que não sei o que, não sei o que lá', 50);
 
-
-
+//
 
 
